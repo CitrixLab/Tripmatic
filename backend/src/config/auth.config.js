7d6470b0f753216f10fdab436dabf4d98f080a17
abstract⁷ <lexusvp001@gmail.com>
2024-11-25 13:28:15 +0800

    Add files via upload

--- backend/src/config/auth.config.js
+++ backend/src/config/auth.config.js
@@ -1,34 +1,31 @@
-
-// ***ATTENTION FOR UST STUDENTS ONLY***
-//
-// FOR CYBER SECURITY COMPLIANCE
-// HTTPS AND SSL CERTIFICATE IS A MUST
-// COMPLY SHA256/512 ENCRYPTION
-//   ASSEMBLY LOW PROGRAMMING LANGUAGE FOR BACKEND SERVER
-//   SYTANX CODE FOR SECURING SECRET ACCESS KEYS & JWT TOKEN
-//   (FOR ENTERPRISE SECURED ENCRIPTION METHOD)
-//
-// Author: Jason "Soo Ji" Dano(citrixlabph@gmail.com)
-
 module.exports = {
   ses: {
-    emailTo: "no-reply@tripmatic.online",
-    source: "no-reply@tripmatic.online",
-    region: "us-east-1",
-    accessKeyId: "YOUR_ACCESS_KEY_ID", // data security compromised
-    secretAccessKey: "YOUR_SECRET_ACCESS_KEY_ID", // data security compromised
+    emailTo: process.env.SES_EMAIL_TO,
+    source: process.env.SES_SOURCE,
+    region: process.env.SES_REGION,
+    accessKeyId: process.env.SES_ACCESS_KEY_ID,
+    secretAccessKey: process.env.SES_SECRET_ACCESS_KEY,
   },
   s3: {
-    bucketName: "tripmatic-bucket",
-    region: "us-east-1",
-    accessKeyId: "YOUR_ACCESS_KEY_ID", // data security compromised
-    secretAccessKey: "YOUR_SECRET_ACCESS_KEY_ID", // data security compromised
+    bucketName: process.env.S3_BUCKET_NAME,
+    region: process.env.S3_REGION,
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
   },
   environtment: "development",
-  db_name: "tripmatic",
-  db_host: "127.0.0.1",
-  db_password: "", // no database password setting // data security compromised
-  db_user: "root",
-  JWT_SECRET: "YOUR_JWT_SECRET_TOKEN", // data security compromised
-  CLIENT_URL: "http://localhost:3000"
-}
+  db_name: process.env.DB_NAME,
+  db_host: process.env.DB_HOST,
+  db_password: process.env.DB_PASSWORD,
+  db_user: process.env.DB_USER,
+  JWT_SECRET: process.env.JWT_SECRET,
+  CLIENT_URL: process.env.CLIENT_URL,
+
+  // reCAPTCHA Config
+  recaptcha: {
+    siteKey: process.env.RECAPTCHA_SITE_KEY,
+    secretKey: process.env.RECAPTCHA_SECRET_KEY,
+  },
+
+  // OpenAI API Config
+  openaiApiKey: process.env.OPENAI_API_KEY,  // Access the OpenAI API key
+};
--- backend/src/config/db-helper.js
+++ backend/src/config/db-helper.js
@@ -9,6 +9,8 @@ const environment = auth.environtment || 'development';
 function setupDb() {
     const db = knex(knexfile[environment]);
     Model.knex(db);
+    console.log(knexfile[environment].connection);
+    db.raw('SELECT 1').then(() => console.log('Database connected')).catch(console.error);
 }
 
 module.exports = setupDb;
