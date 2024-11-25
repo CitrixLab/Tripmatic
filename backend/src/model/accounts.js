8553e0bd84c71995c4d9495ed0e052a5c94ecbea
abstract⁷ <lexusvp001@gmail.com>
2024-11-25 13:30:56 +0800

    Add files via upload

--- backend/src/model/accounts.js
+++ backend/src/model/accounts.js
@@ -1,4 +1,5 @@
 const BaseModel = require('./base-model');
+
 class Accounts extends BaseModel {
   static get tableName() {
     return "accounts";
@@ -7,25 +8,27 @@ class Accounts extends BaseModel {
   $beforeInsert() {
     this.created_at = new Date();
   }
-  static async addUser (data) {
+
+  static async addUser(data) {
     return this.query().insert(data);
   }
 
   static async getUserByEmail(email) {
-    return this.query().findOne({email: email});
+    return this.query().findOne({ email: email });
   }
-  static async emailExists (email) {
-    const data = await this.query().where({email: email}).resultSize();
+
+  static async emailExists(email) {
+    const data = await this.query().where({ email: email }).resultSize();
     return data > 0;
   }
-  
-  static async checkLoggedIn (email, password) {
-    const numberRow = await this.query().where({email: email})
-      .andWhere({password: password})
+
+  static async checkLoggedIn(email, password) {
+    const numberRow = await this.query().where({ email: email })
+      .andWhere({ password: password })
       .resultSize();
-    const data = await this.query().where({email: email})
-      .andWhere({password: password})
-      .andWhere({isVerified: 1})
+    const data = await this.query().where({ email: email })
+      .andWhere({ password: password })
+      .andWhere({ isVerified: 1 })
       .first();
     return {
       isExist: numberRow > 0,
@@ -33,7 +36,7 @@ class Accounts extends BaseModel {
     };
   }
 
-  static async getUserById(id){
+  static async getUserById(id) {
     return this.query().findById(id);
   }
 
@@ -41,15 +44,45 @@ class Accounts extends BaseModel {
     return this.query().patch(data).findById(id);
   }
 
-  static async updateSingleUserCustom (data, where) {
+  static async updateSingleUserCustom(data, where) {
     await this.query().patch(data).findOne(where);
     return this.query().findOne(where);
   }
 
-  static async accountExists (data) {
+  static async accountExists(data) {
     const result = await this.query().where(data).resultSize();
     return result > 0;
   }
+
+  // New method to change the password
+/*
+  static async changePassword(userEmail, newPassword) {
+    if (!userEmail || !newPassword) {
+      throw new Error("Email or new password is missing");
+    } 
+    return this.query()
+      .patch({ password: newPassword })
+      .where('email', userEmail)  // Use email instead of userId 
+  }
+*/
+    static async changePassword(userEmail, newPassword) {
+      if (!userEmail || !newPassword) {
+        throw new Error("Email or new password is missing");
+      } 
+      
+      return this.knex().transaction(async (trx) => {
+        const updatedRows = await this.query(trx)
+          .patch({ password: newPassword })
+          .where('email', userEmail);
+        
+        if (updatedRows === 0) {
+          throw new Error("User not found or password not changed");
+        }
+        
+        return true;
+      });
+    }
+    
 }
 
 module.exports = Accounts;
